efc96b59834489167a1ec009a8884efa5492c84e
poopoothegorilla <jm.wlkr1@gmail.com>
2014-11-11 14:57:05 -0500

    add show view and action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,9 +27,14 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(params[:user])
+
+    redirect_to "/users"
   end
 
   def destroy
