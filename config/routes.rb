0a7c7cccd0a9e78b7d68daa1db5859d0e85d5f26
poopoothegorilla <jm.wlkr1@gmail.com>
2014-11-11 11:20:36 -0500

    add users controller and routes and associated views and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/create'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
